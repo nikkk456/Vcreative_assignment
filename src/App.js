fa19d0bf07a7c2d21f4195d02c019384ec0376af
Nikhil Mishra <104861212+nikkk456@users.noreply.github.com>
2024-05-22 00:50:19 +0530

    Fixed Margin

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
     <center><h1 className='text-black mt-4 mb-2 text-4xl font-bold'>DASHBOARD</h1></center>
-    <div className='bg-white mb-9 mx-9 rounded-xl border-2' style={{boxShadow:"0px 0px 8px 2px grey"}}>
+    <div className='bg-white mb-9 rounded-xl border-2 margin-side' style={{boxShadow:"0px 0px 8px 2px grey"}}>
       <Home data = {data}/>
     </div>
     </>
